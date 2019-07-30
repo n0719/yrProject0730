254bd494840063f1e7eaa40bff2944945ae58583
GXT <Passerby_T@163.com>
2019-07-30 09:57:46 +0800

    登录页

--- yrproject/src/components/user/login/Login.vue
+++ yrproject/src/components/user/login/Login.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="login">
-        login
+        <div class="bounced">
+             <div class="bounced-header">登录</div>
+         </div>
     </div>
 </template>
 <script>
@@ -10,6 +12,19 @@ export default {
 </script>
 <style>
  .login{position:fixed;width:100%;top:0;bottom:0;left:0;background:rgba(0,0,0,0.5);text-align:Center;z-index:5;} 
+ .bounced{
+     position: absolute;
+     left: 50%;
+     top: 50%;
+     transform:translate(-50%,-50%);
+     width: 450px;
+     height: 540px;
+     background: #fff;
+ }
+ .bounced-header{
+     background: #2B3643;
+     color: #E6CF68;
+ }
 </style>
 
 
