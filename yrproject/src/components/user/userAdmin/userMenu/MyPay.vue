9ba623a8d009db14067e8cee5e8a7b7c4de7595b
kevinonly3@163.com <kevinonly3@163.com>
2019-07-31 12:08:42 +0800

    1

--- yrproject/src/components/user/userAdmin/userMenu/MyPay.vue
+++ yrproject/src/components/user/userAdmin/userMenu/MyPay.vue
@@ -3,7 +3,7 @@
   <div class="myPay">
     <div class="myPayTop">
       <el-row class="container">
-        <el-row class="payNav"><span></span> <a>ss</a></el-row>
+        <el-row class="payNav"><span></span>我的账户</el-row>
         <el-row>
           <el-col :span="6">
             <span>余额(元)</span>
@@ -24,6 +24,11 @@
         </el-row>
       </el-row>
     </div>
+      <div class="myPayContent">
+      <el-row class="container">
+        <el-row class="payNav"><span></span>我的账户</el-row>
+      </el-row>
+    </div>
   </div>
 </template>
 <script>
@@ -36,18 +41,22 @@ export default {
 <style>
 .myPay {
   padding: 20px;
+  display:flex;
+  flex-direction: column;
+  height:100%;
+  box-sizing: border-box;
 }
-.payNav{display:flex;}
-.payNav span{height:20px;border-right:1px solid #ddd;}
+.myPayTop{margin-bottom:20px;height:186px; background: #fff;}
+.payNav{display:flex;padding-left:35px;align-items: center;color:#836426; padding:18px 0 0 35px;}
+.payNav span{height:14px;border-right:4px solid #836426;margin-right:5px;}
 .myPayTop .container {
   height: 168px;
-  background: #fff;
   display: flex;
   flex-direction: column;
 }
 .myPayTop .container .el-row:nth-of-type(1) {
-  height: 38px;
-  line-height: 56px;
+  /* padding:18px 0 0 35px; */
+ 
 }
 .myPayTop .container .el-row:nth-of-type(2) {
   flex: 1;
@@ -58,9 +67,10 @@ export default {
   align-items: center;
   justify-content: center;
   height: 60%;
-  border-right:1px solid #ddd;
+  border-right:1px solid #707070;
   flex-direction: column;
 }
+.myPayTop .el-col-6:nth-of-type(4){border:none;}
 .myPayTop .el-col-6 span {
   text-align: center;
   letter-spacing: 1px;
@@ -70,6 +80,8 @@ export default {
   font-size: 34px;
   color: #836426;
 }
+
+.myPayContent{background:#fff;flex:1;}
 </style>
 
 
